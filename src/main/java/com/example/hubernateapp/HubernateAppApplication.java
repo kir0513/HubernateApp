3ebd2a496099ec2eecf06ee5c327b9a787d6ddef
kir0513 <kir0513@yandex.ru>
2024-03-24 16:23:11 +0300

    Initial commit. Работа с БД

---
+++ src/main/java/com/example/hubernateapp/HubernateAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hubernateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubernateAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubernateAppApplication.class, args);
+    }
+
+}
